358bb5bfcfa0c51bf89bb0d82a6b12c41b181b90
Maykel Baragano <maykel.baragano@gmail.com>
2026-03-05 14:32:09 -0600

    Started creating parser class and main function

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,18 @@
 import logging
+import json
+
+json_file = "json_file.json"
+
+def read_json_file():
+    filename = "json_file.json"
+    try:
+        with open (filename, "r") as file:
+            data = json.load(file)
+            logger.info(f"Data was loaded: {data}")
+    except FileNotFoundError as e:
+        logger.error(f"File nowt found error: {e}")
+
+
 
 #Create the logger
 logger = logging.getLogger("MyLogger")
@@ -10,5 +24,39 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-#Testing the logger
-logger.info("Testing the logger")
+
+#Function to test logger on all levels
+def test_logger_levels():
+    logger.info("Info level log")
+    logger.debug("Debug level log")
+    logger.warning("Warning level log")
+    logger.error("Error level log")
+    logger.critical("Critical level log")
+
+
+#Creating a class to parse logs
+class Parser():
+        
+    def __init__(self, log_file):
+        self.log_file = log_file
+        self.log_data = self.return_logs()
+        print(f"{self.log_data}, {log_file}")
+    
+    def return_logs(self):
+        logs = []
+        with open(self.log_file, "r") as file:
+            line = file.readline()
+            while line:
+                log = json.loads(line)
+                for info in log.keys():
+                    print(info, log.keys)
+
+log_file = "test.log"
+
+def main():
+    my_parser = Parser(log_file)
+    my_parser.return_logs()
+
+
+if __name__ == '__main__':
+    main()
